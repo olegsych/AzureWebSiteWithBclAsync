11d1b9e9450c5daaca68a114bb88512b34449873
Oleg Sych <olegsych@microsoft.com>
2015-04-16 14:44:26 -0700

    - Add custom event tracking to the HomeController.

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly TelemetryClient telemetryClient = new TelemetryClient();
+
         public ActionResult Index()
         {
+            telemetryClient.TrackEvent("Index");
+
             return View();
         }
 
         public ActionResult About()
         {
+            telemetryClient.TrackEvent("About");
+
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -22,6 +29,8 @@ namespace WebApplication1.Controllers
 
         public ActionResult Contact()
         {
+            telemetryClient.TrackEvent("Contact");
+
             ViewBag.Message = "Your contact page.";
 
             return View();
