fa304e8d59eae1a5851f2882cf35fec30a29d848
Oleg Sych <olegsych@microsoft.com>
2015-04-17 10:17:46 -0700

    - Change repro project from MVC to WebPages trying to get to a simplest repro.

---
+++ WebApplication1/ClassLibrary1/Net40Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class Net40Class
+    {
+        public static void DoAsyncStuff()
+        {
+            TaskEx.Run(() => Thread.Sleep(42));
+        }
+    }
+}
